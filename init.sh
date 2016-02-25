d626ff5f63ad7f8f2686f3133ff6f0c59c21413d
Eric Krawczyk <eric@docnetwork.org>
2016-02-25 10:36:02 -0500

    surprise more init

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@
 set -x
 exec > >(tee /home/timmy/init.log) 2>&1
 
+whoami
+
 # Node
 curl -# -L https://nodejs.org/dist/v4.2.6/node-v4.2.6-linux-x64.tar.gz \
   | sudo tar xzf - --strip-components=1 -C /usr/local
