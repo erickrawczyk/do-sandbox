572c2fee294226235db9cd6390229dc7824d29fe
Eric Krawczyk <eric@docnetwork.org>
2016-02-25 10:25:20 -0500

    init stuff. again.

--- init.sh
+++ init.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-touch $HOME/testing.txt
+set -x
+exec > >(tee /home/timmy/init.log) 2>&1
 
-touch /test.txt
+# Node
+curl -# -L https://nodejs.org/dist/v4.2.6/node-v4.2.6-linux-x64.tar.gz \
+  | sudo tar xzf - --strip-components=1 -C /usr/local
+
+export HOME=/home/timmy
+
+touch $HOME/testing.txt
