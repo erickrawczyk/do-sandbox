aa2a77a9d91db540cf4e6bd214599e670cb2b4ad
Eric Krawczyk <eric@docnetwork.org>
2016-03-10 19:30:35 -0500

    rename timmy

--- init.sh
+++ init.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -x
-exec > >(tee /home/timmy/init.log) 2>&1
-
-whoami
+exec > >(tee /home/ubuntu/init.log) 2>&1
 
 # Node
 curl -# -L https://nodejs.org/dist/v4.2.6/node-v4.2.6-linux-x64.tar.gz \
   | sudo tar xzf - --strip-components=1 -C /usr/local
 
-export HOME=/home/timmy
+export HOME=/home/ubuntu
 
 touch $HOME/testing.txt
